6ebb43468f8ebb2c54906edded75a4b698b60dc5
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2024-04-24 15:49:56 +0200

    Esercizio 1 ADD Classe Cane

---
+++ csharp-abstract-animals/Cane.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_abstract_animals
+{
+    internal class Cane : Animale
+    {
+        public override string Cibo { get; set; } = "Carne";
+
+        public override string Verso()
+        {
+            return "Bau Bau";
+        }
+    }
+}

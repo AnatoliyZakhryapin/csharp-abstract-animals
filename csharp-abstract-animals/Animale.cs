13030eeb28a48808196444db838bf52df526b40f
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2024-04-24 15:49:44 +0200

    Esercizio 1 ADD Classe Animale

---
+++ csharp-abstract-animals/Animale.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_abstract_animals
+{
+    internal abstract class Animale
+    {
+        public abstract string Cibo { get; set; }
+        public string Dormi()
+        {
+            return "Zzz";
+        }
+        public abstract string Verso();
+
+        public string Mangia()
+        {
+            return Cibo;
+        }
+    }
+}

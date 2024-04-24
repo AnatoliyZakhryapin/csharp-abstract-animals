a996e7a61749fdcbd94c054fa67865234606a1d8
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2024-04-24 15:50:18 +0200

    Esercizio 1 ADD Classe Delfino

---
+++ csharp-abstract-animals/Delfino.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_abstract_animals
+{
+    internal class Delfino : Animale
+    {
+        public override string Cibo { get; set; } = "Pesce";
+
+        public override string Verso()
+        {
+            return "Verso di Delfino";
+        }
+    }
+}

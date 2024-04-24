ccab36f84c833aa7fa8df4a1c079bb97db6af616
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2024-04-24 15:52:28 +0200

    Esercizio 1 ADD to programm List of Animal and try all method of each Animal

--- csharp-abstract-animals/Program.cs
+++ csharp-abstract-animals/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            List<Animale> listaAnimali = new List<Animale>();
+            listaAnimali.Add(new Cane());
+            listaAnimali.Add(new Passerotto());
+            listaAnimali.Add(new Aquila());
+            listaAnimali.Add(new Delfino());
+
+            foreach (var animale in listaAnimali)
+            {
+                Console.WriteLine($"Io sono: {animale.GetType().Name}");
+                Console.WriteLine($"Il mio verso è: {animale.Verso()}");
+                Console.WriteLine($"Quando dormo faccio: {animale.Dormi()}");
+                Console.WriteLine($"Io mangio: {animale.Mangia()}");
+                Console.WriteLine();
+            }
+
         }
     }
 }
